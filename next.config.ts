d1e64bbbfafebb8ec5f3aae6cc85550ebaa55909
kinannwidya <kinantikinan2662@gmail.com>
2026-02-10 11:30:11 +0700

    chore: remove deprecated eslint config for Next 16

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // kosong juga gapapa
 };
 
 export default nextConfig;
